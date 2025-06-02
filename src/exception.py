90e98384f93a9ce5213a98923a134f76aad723b1
Volcanogenesis <aditya.ranjan1712@gmail.com>
2025-06-02 18:54:25 +0530

    EDA and Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-
+from src.logger import logging 
 def error_message_detail(error, error_detail:sys):
     _,_,exe_tb = error_detail.exc_info()
     error_message = "Error occured in python script name[{0}] line number[{1}] error message[{2}]".format(
